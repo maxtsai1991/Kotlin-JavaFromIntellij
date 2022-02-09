8de19a322d6865b1b6f1e05f5c24d065a57e94ee
MaxTsai <maxtsai@esobi.com>
2022-02-09 11:35:57 +0800

    hahow Java & Kotlin 一起學 : 單元13 實務案例,設計具有屬性、建構子死方法的類別，整合所學

---
+++ src/main/java/com/max/student/SchoolRunner.java
@@ -0,0 +1,9 @@
+package com.max.student;
+
+public class SchoolRunner {
+    public static void main(String[] args) {
+    Student stu = new Student("Max",60,77);
+    stu.print();
+
+    }
+}
---
+++ src/main/kotlin/com/max/student/Student.kt
@@ -0,0 +1,14 @@
+package com.max.student
+
+
+
+fun main(args: Array<String>) {
+    val stu = Student("Jack",66,88)
+    stu.print()
+}
+
+class Student (var name : String , var english : Int , var math : Int) {
+    fun print(){
+        println( name + "\t" + english + "\t" + math + "\t" + ( english + math ) / 2 )
+    }
+}
