20036d7cbe422f290c274e59df76df87f0afa84b
MaxTsai <maxtsai@esobi.com>
2022-02-25 16:34:28 +0800

    hahow Java & Kotlin 一起學 : 單元27 Set與Map集合,計算多筆停車費 (增加註解,以利辨識)

--- src/main/kotlin/com/max/Many.kt
+++ src/main/kotlin/com/max/Many.kt
@@ -3,6 +3,7 @@ package com.max
 import java.time.LocalDateTime
 
 fun main() {
+    println("-----HashMap練習,計算多筆停車費------")
     // 用車牌方式找到車,用HashMap
     val parkingLot = HashMap<String, Car?>() // 建立一個停車場物件 可以存放Key是字串,Value是Car物件的一個資料集合, ?代表可以接受null值
     var enter = LocalDateTime.of(2018,12,25,8,0,0) // 設定進場時間
@@ -31,22 +32,24 @@ fun main() {
     println("停車場目前停放數輛 : " +parkingLot.size)
 
 
+
     /*
     Kotlin 裡面
     list 是不可變的 , 意思是不能用".add"去添加list裡面的數值
      */
-//    val list  = listOf(5,1,2,7)
-//    println("印出list:$list")
-//
-//    val scores = listOf(68,88,77,99,50) //不可變list
-//    for(score in scores){
-//        println("kotlin不可寫入的List: $score")
-//    }
-//
-//    println("list變數第3個位置的數字 : " + list.get(2))
-//
-//    var mutableList : MutableList<Int> = mutableListOf(5,1,2,7) //可變(可寫)list , 也可以寫成 var mutableList = mutableListOf(5,1,2,7), 因kotlin 會自行推斷 等號右邊的型別
-//    mutableList.add(6)
-//    println("kotlin可寫入的List: $mutableList" )
+    println("-----list練習------")
+    val list  = listOf(5,1,2,7)
+    println("印出list:$list")
+
+    val scores = listOf(68,88,77,99,50) //不可變list
+    for(score in scores){
+        println("kotlin不可寫入的List: $score")
+    }
+
+    println("list變數第3個位置的數字 : " + list.get(2))
+
+    var mutableList : MutableList<Int> = mutableListOf(5,1,2,7) //可變(可寫)list , 也可以寫成 var mutableList = mutableListOf(5,1,2,7), 因kotlin 會自行推斷 等號右邊的型別
+    mutableList.add(6)
+    println("kotlin可寫入的List: $mutableList" )
 
 }
