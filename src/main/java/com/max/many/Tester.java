a3dfddfb80e24d5bf12a83e87afecc9c1e85c5c5
MaxTsai <maxtsai@esobi.com>
2022-02-25 16:30:42 +0800

    hahow Java & Kotlin 一起學 : 單元27 Set與Map集合,計算多筆停車費

--- src/main/java/com/max/many/Tester.java
+++ src/main/java/com/max/many/Tester.java
@@ -1,11 +1,53 @@
 package com.max.many;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Tester {
     public static void main(String[] args) {
+        /**
+         * ctrl + alt + m 從MAIN抽取出來方法快捷鍵
+         */
+        map();          // Map<Key,Value> 特性: 1.一筆資料裡面包含兩樣東西,一個是Key(鍵值),一個是鍵值所對應到的值叫Value 2.不能重複(有重複的話,只會印出重複的一筆) 3. 不帶有資料放入的順序
+        set();          // Set 特性: 1.不希望有任何重複性的資料,可以避免再去檢查裏頭有沒有重複(如有重複數值,只會顯示一個) 2. 沒有index值(沒有次序的概念,不能用index值去取得某一筆資料) 3.HashSet沒有排序功能,TreeSet才有排序功能
+        list();         // List 特性: 1.順序 2.索引 3.可以放重覆性的資料 ; 加<Integer>代表只能放Integer整數
+        arrayTest();    // Array
+
+    }
+
+    private static void map() {
+        HashMap<String,String> stocks = new HashMap<>(); // HashMap<String,String> stocks股票當範例 : < 股票的股號(EX:0050) , 公司的名稱(EX:台積電) >
+        // Map放值用.put
+        stocks.put("2330","台積電");
+        stocks.put("2330","TSMC");// key要是一樣 ,印出的Value 會覆蓋成新的【台積電 > TSMC】
+        stocks.put("2317","鴻海");
+        stocks.put("2317","鴻海");// 不能重複(有重複的話,只會印出重複的一筆)
+
+        // 印出Map 裡的全部資料
+        System.out.println("印出Map : " + stocks);
+
+        // Map 取單一筆的值 用.get方法
+        System.out.println(" 取得2330的字串值 : " + stocks.get("2330"));
+
+        // Map 取出所有得key 及 value 值 ,用 for迴圈
+        for (String key : stocks.keySet()){ // 先用for迴圈取得key, 在利用key值呼叫stocks的get方法,然後再得到所有的Value值
+            System.out.println("印出股票Map所有Key字串 : " + key + ", 及所有Value字串 : " + stocks.get(key));
+        }
+    }
+
+    private static void set() {
+        HashSet<Integer> set = new HashSet<>();
+        set.add(6);
+        set.add(7);
+        set.add(1);
+        set.add(8);
+        set.add(7);
+        System.out.println("印出HashSet : " + set);
+        for (int n : set){
+            System.out.println("取出HashSet裡的的所有值 : " + n);
+        }
+    }
+
+    private static void list() {
         ArrayList<Integer> list = new ArrayList(); // List的特性: 1.順序 2.索引 3.可以放重覆性的資料 ; 加<Integer>代表只能放Integer整數
         list.add(5);
         list.add(3);
@@ -13,6 +55,7 @@ public class Tester {
         list.add(7);
         list.add(1);
         list.add(1);
+
         System.out.println("印出list : " + list);
         int n1 = list.get(0);
         int n2 = list.get(3);
@@ -23,8 +66,6 @@ public class Tester {
         for(int score : scores){
             System.out.println(score);
         }
-        arrayTest(); // array練習抽取出來的方法
-
     }
 
     private static void arrayTest() {
--- src/main/kotlin/com/max/Many.kt
+++ src/main/kotlin/com/max/Many.kt
@@ -1,19 +1,52 @@
 package com.max
 
+import java.time.LocalDateTime
+
 fun main() {
+    // 用車牌方式找到車,用HashMap
+    val parkingLot = HashMap<String, Car?>() // 建立一個停車場物件 可以存放Key是字串,Value是Car物件的一個資料集合, ?代表可以接受null值
+    var enter = LocalDateTime.of(2018,12,25,8,0,0) // 設定進場時間
+
+    var car : Car? = Car("AAA-0001",enter) // 第一台車, ?代表可以接受null值
+    parkingLot.put(car!!.id,car) // 停車場停了第一台車 , key是車牌 , value是car物件(包含車牌及進場時間) ,兩個!!代表如果key是null是可以接受的
+
+    car = Car("BBB-0002",enter.plusMinutes(15)) // 第二台車進場時間 ,enter.plusMinutes(15) : 第一台車的時間往後加15分鐘
+    parkingLot.put(car.id,car) // 停車場停了第二台車 , key是車牌 , value是car物件(包含車牌及進場時間)
+
+    println("停車場目前停放數輛 : " + parkingLot.size)
+
+    // 第一台車離開時間 (Car 1 Leaving) 進場時間: 8點整 ,離場時間: 9點整
+    var leave = LocalDateTime.of(2018,12,25,9,0,0) // 設定離場時間
+    car = parkingLot.get("AAA-0001")
+    car?.leave = leave
+    println("${car?.id} 這台車停了多久時間(單位:分鐘,英譯:duration): ${car?.duration()}")
+    parkingLot.remove(car?.id) // 第一台車離開停車場需移除
+    println("停車場目前停放數輛 : " + parkingLot.size)
+
+    // 第二台車離開時間 (Car 2 Leaving) 進場時間: 8點15分 ,離場時間: 11點
+    car = parkingLot.get("BBB-0002")
+    car?.leave = leave.plusHours(2) // 假設第二台車 兩個小時後出場 , .plusHours(2) : leave時間 往後加兩小時
+    println("${car?.id} 這台車停了多久時間(單位:分鐘,英譯:duration): ${car?.duration()}")
+    parkingLot.remove(car?.id) // 第二台車離開停車場需移除
+    println("停車場目前停放數輛 : " +parkingLot.size)
+
+
     /*
     Kotlin 裡面
     list 是不可變的 , 意思是不能用".add"去添加list裡面的數值
      */
-    val list  = listOf(5,1,2,7)
-    println("印出list:$list")
-    val scores = listOf(68,88,77,99,50) //不可變list
-    for(score in scores){
-        println("kotlin不可寫入的List: $score")
-    }
-    println("list變數第3個位置的數字 : " + list.get(2))
+//    val list  = listOf(5,1,2,7)
+//    println("印出list:$list")
+//
+//    val scores = listOf(68,88,77,99,50) //不可變list
+//    for(score in scores){
+//        println("kotlin不可寫入的List: $score")
+//    }
+//
+//    println("list變數第3個位置的數字 : " + list.get(2))
+//
+//    var mutableList : MutableList<Int> = mutableListOf(5,1,2,7) //可變(可寫)list , 也可以寫成 var mutableList = mutableListOf(5,1,2,7), 因kotlin 會自行推斷 等號右邊的型別
+//    mutableList.add(6)
+//    println("kotlin可寫入的List: $mutableList" )
 
-    var mutableList : MutableList<Int> = mutableListOf(5,1,2,7) //可變(可寫)list , 也可以寫成 var mutableList = mutableListOf(5,1,2,7), 因kotlin 會自行推斷 等號右邊的型別
-    mutableList.add(6)
-    println("kotlin可寫入的List: $mutableList" )
 }
